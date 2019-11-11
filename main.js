d6e78c0169f6fbeca3698c1c210738c3a55820a6
Harley <Zonkeystripes@hotmail.com>
2019-11-11 13:32:28 -0700

    Taking break. Need to had validating password stuff.

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+//DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercasetEl = document.getElementById('result');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol,
+};
+
+// generateEl.addEventListener('click', () => {
+//     const length = lengthEl.value;
+
+//     console.log(length);
+// })
+
+
+
+
 // Generator functions - http://www.net-comber.com/charset.html
 // The numbers represent the variable they are assigned to, via the chart from the website. Numbers 97-121 are lower case letters. Hence; 97 + 26.) THe Math.floor rounds a decimal to the nearst whole, and the Math.random generates a random decimal number. This is where the random letters are comming from. Generating numbers between 97 and 121 that represent lowercase letters.
 function getRandomLower() {
@@ -16,6 +43,10 @@ function getRandomSymbol(){
    const symbols = ' !"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+
+
+
 
 console.log(getRandomUpper());
 console.log(getRandomLower());
